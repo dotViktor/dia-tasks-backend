0eda7937bc3d8eff2f10e8d44c22284baccb56bc
dotViktor <viktorstoimenov@vivaldi.net>
2023-11-13 14:31:45 +0200

    Working GetUsers

--- app/routes/users.routes.js
+++ app/routes/users.routes.js
@@ -3,8 +3,8 @@ import { usersController } from "../controllers/users.controllers.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.status(200).send(usersController.getUsers());
+router.get("/", async (req, res) => {
+  res.status(200).send(await usersController.getUsers());
 });
 
 export { router as usersRouter };
