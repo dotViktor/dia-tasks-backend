50cb90e3a84fb175c8d10afad6d14411f1620dca
dotViktor <viktorstoimenov@vivaldi.net>
2023-11-13 16:32:46 +0200

    Added Cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { usersRouter } from "./app/routes/users.routes.js";
 import * as dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const app = express();
 //middlewares
 app.use("/users", usersRouter);
 app.use(express.json());
+app.use(cors());
 
 app.listen(7777, () => {
   console.log("Server started on port 7777");
