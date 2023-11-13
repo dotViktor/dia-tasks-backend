fad44d81fea70f45ffacc6d755dbc53ca009f277
dotViktor <viktorstoimenov@vivaldi.net>
2023-11-13 16:08:03 +0200

    Added getUserById

--- app/controllers/users.controllers.js
+++ app/controllers/users.controllers.js
@@ -12,4 +12,32 @@ export async function getUsers() {
   }
 }
 
+export async function getUsersById(id) {
+  try {
+    const [rows] = await dataBase.query(
+      `
+    SELECT * FROM Users WHERE id = ?;
+    `,
+      [id]
+    );
+    return rows;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function addUser(name, email, role, password) {
+  try {
+    const [rows] = await dataBase.query(
+      `
+    INSERT INTO Users (name, email, role, password) VALUES (?, ?, ?, ?);
+    `,
+      [name, email, role, password]
+    );
+    return rows;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 export * as usersController from "./users.controllers.js";
--- app/routes/users.routes.js
+++ app/routes/users.routes.js
@@ -7,4 +7,8 @@ router.get("/", async (req, res) => {
   res.status(200).send(await usersController.getUsers());
 });
 
+router.get("/:id", async (req, res) => {
+  res.status(200).send(await usersController.getUsersById(req.params.id));
+});
+
 export { router as usersRouter };
