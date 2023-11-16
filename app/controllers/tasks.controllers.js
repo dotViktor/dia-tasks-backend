7134afb0eb8fef4c13aeccf005b70a110a5a4d88
dotViktor <viktorstoimenov@vivaldi.net>
2023-11-16 16:52:23 +0200

    Create Update Delete Assign

--- app/controllers/tasks.controllers.js
+++ app/controllers/tasks.controllers.js
@@ -20,4 +20,51 @@ export async function getTaskById(id) {
   }
 }
 
+export async function createTask(title, description, startTime, endTime) {
+  try {
+    const [rows] = await dataBase.query(
+      `INSERT INTO Tasks (title, description, startTime, endTime) VALUES (?, ?, ?, ?);`,
+      [title, description, startTime, endTime]
+    );
+    return rows;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function updateTask(id, title, description, startTime, endTime) {
+  try {
+    const [rows] = await dataBase.query(
+      `UPDATE Tasks SET title = ?, description = ?, startTime = ?, endTime = ? WHERE id = ?;`,
+      [title, description, startTime, endTime, id]
+    );
+    return rows;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function deleteTaskById(id) {
+  try {
+    const [rows] = await dataBase.query(`DELETE FROM Tasks WHERE id = ?;`, [
+      id,
+    ]);
+    return rows;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function assignUserToTask(userId, taskId) {
+  try {
+    const [rows] = await dataBase.query(
+      `INSERT INTO UserTasks (UserID, TaskID) VALUES (?, ?);`,
+      [userId, taskId]
+    );
+    return rows;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 export * as tasksController from "./tasks.controllers.js";
--- app/routes/tasks.routes.js
+++ app/routes/tasks.routes.js
@@ -9,4 +9,48 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
   res.status(200).send(await tasksController.getTaskById(req.params.id));
 });
+
+router.post("/", async (req, res) => {
+  if (!req.body.title || !req.body.description || !req.body.startTime) {
+    res.status(400).send("Missing required fields");
+    return;
+  }
+  const response = await tasksController.createTask(
+    req.body.title,
+    req.body.description,
+    req.body.startTime,
+    req.body.endTime
+  );
+  res.status(201).send(response);
+});
+
+router.put("/:id", async (req, res) => {
+  if (!req.body.title || !req.body.description || !req.body.startTime) {
+    res.status(400).send("Missing required fields");
+    return;
+  }
+  const response = await tasksController.updateTask(
+    req.params.id,
+    req.body.title,
+    req.body.description,
+    req.body.startTime,
+    req.body.endTime
+  );
+  res.status(200).send(response);
+});
+
+router.delete("/:id", async (req, res) => {
+  res.status(200).send(await tasksController.deleteTaskById(req.params.id));
+});
+
+router.post("/:taskId/add-user/:userId", async (req, res) => {
+  res
+    .status(200)
+    .send(
+      await tasksController.assignUserToTask(
+        req.params.userId,
+        req.params.taskId
+      )
+    );
+});
 export { router as tasksRouter };
